f46cb05af272432d53d07f4d9cd9b7d3ec9eb8d2
Ashutosh Moharana <ashutoshmoharana00@gmail.com>
2025-05-26 12:10:33 +0530

    change petpost image storage to cloudinary

--- config/multer.js
+++ config/multer.js
@@ -1,32 +1,9 @@
 const multer = require("multer");
-const crypto = require("crypto");
 const path = require("path");
+const os = require("os");
 
-const petPostStorage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/uploads/petImages");
-  },
-  filename: function (req, file, cb) {
-    const fn = crypto.randomBytes(10, (err, bytes) => {
-      const fn = bytes.toString("hex") + path.extname(file.originalname);
+// Use OS temp directory for uploads (since files are immediately uploaded to Cloudinary)
+const petUpload = multer({ dest: os.tmpdir() });
+const userUpload = multer({ dest: os.tmpdir() });
 
-      cb(null, fn);
-    });
-  },
-});
-const userPicStorage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/uploads/userImages");
-  },
-  filename: function (req, file, cb) {
-    const fn = crypto.randomBytes(10, (err, bytes) => {
-      const fn = bytes.toString("hex") + path.extname(file.originalname);
-
-      cb(null, fn);
-    });
-  },
-});
-
-const petUpload = multer({ storage: petPostStorage });
-const userUpload = multer({ storage: userPicStorage });
 module.exports = { petUpload, userUpload };
--- controllers/postController.js
+++ controllers/postController.js
@@ -2,6 +2,14 @@ const PetPost = require("../models/petpost");
 const userModel = require("../models/user");
 const fs = require('fs');
 const path = require('path');
+const cloudinary = require('cloudinary').v2;
+
+// Cloudinary config (use env variables in production)
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const getPosts = async (req, res) => {
   try {
@@ -15,82 +23,65 @@ const getPosts = async (req, res) => {
 
 const createPost = async (req, res) => {
   let user = await userModel.findOne({ email: req.user.email });
-
-  console.log(user);
-
   const { title, petName, petType, description, type, createdAt } = req.body;
+  let imageUrl = '';
+  if (req.file) {
+    // Upload to Cloudinary
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: 'petImages',
+      resource_type: 'image',
+    });
+    imageUrl = result.secure_url;
+    // Optionally, delete local file after upload
+    fs.unlink(req.file.path, () => {});
+  }
   const post = await PetPost.create({
     title,
     petName,
     petType,
     description,
     type,
-    picture: "/uploads/petImages/" + req.file.filename,
+    picture: imageUrl,
     userId: user._id,
     createdAt,
   });
-  console.log(req.file.destination + "/" + req.file.filename);
-
   user.posts.push(post._id);
   await user.save();
-  console.log(`Your post is ${post}`);
-  res.json({ msg: "Post created successfully" ,post});
+  res.json({ msg: "Post created successfully", post });
 };
 
 const updatePost = async (req, res) => {
   const { ...data } = req.body;
   let updatedFields = { ...data };
-
-  // If a new file is uploaded, handle old image deletion
   if (req.file) {
-    // Find the existing post to get the old image path
+    // Find the existing post to get the old image URL
     const existingPost = await PetPost.findById(req.params.id);
-    if (existingPost && existingPost.picture) {
-      const oldImagePath = path.join(__dirname, '../public', existingPost.picture);
-      fs.unlink(oldImagePath, (err) => {
-        if (err) {
-          console.error('Error deleting old image file:', err);
-        } else {
-          console.log('Old image file deleted:', oldImagePath);
-        }
-      });
-    }
-    updatedFields.picture = "/uploads/petImages/" + req.file.filename;
+    // Optionally, delete old image from Cloudinary (if you store public_id)
+    // Upload new image to Cloudinary
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: 'petImages',
+      resource_type: 'image',
+    });
+    updatedFields.picture = result.secure_url;
+    fs.unlink(req.file.path, () => {});
   }
-
   let updatedData = await PetPost.findOneAndUpdate(
     { _id: req.params.id },
     updatedFields,
     { new: true }
   );
-
-  console.log(`Your updated data is ${updatedData}`);
   res.json("Your data has been succesfully updated!");
 };
 
 const deletePost = async (req, res) => {
   let user = await userModel.findOne({ email: req.user.email });
   const post = await PetPost.findOneAndDelete({ _id: req.params.id });
-  // Remove post reference from user
   const index = user.posts.findIndex((elem) => elem._id == req.params.id);
   if (index !== -1) {
     user.posts.splice(index, 1);
   }
   await user.save();
-
-  // Delete image file from server if it exists
-  if (post && post.picture) {
-    const imagePath = path.join(__dirname, '../public', post.picture);
-    
-    fs.unlink(imagePath, (err) => {
-      if (err) {
-        console.error('Error deleting image file:', err);
-      } else {
-        console.log('Image file deleted:', imagePath);
-      }
-    });
-  }
-
+  // Optionally, delete image from Cloudinary if you store public_id
   res.json("Your post has been deleted succesfully !");
 };
 module.exports = { getPosts,createPost, updatePost, deletePost };
